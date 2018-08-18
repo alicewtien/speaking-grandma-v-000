2d243503fd785dff468f2b55dd60ca1ca4ca7f18
alice T <alicewtien@gmail.com>
2018-08-18 16:14:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@ def speak_to_grandma(phrase)
 
 # NO, NOT SINCE 1938!
   else
-    return 'I LOVE YOU TOO PUMPKIN!'
+    return 'NO, NOT SINCE 1938!'
   end
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
